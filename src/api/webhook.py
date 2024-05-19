5e8044d67ad7bac6ee1e49b0b216012622cd493f
joaquinreyero <joaquinreyero@hotmail.com>
2024-05-19 00:19:33 -0300

    test webhook

--- src/api/webhook.py
+++ src/api/webhook.py
@@ -5,6 +5,7 @@ from fastapi import Request, HTTPException, Depends, APIRouter
 from src.schema.webhook import WebhookPayload
 from src.decorator.security import signature_required
 from src.utils.whatsapp import process_whatsapp_message, is_valid_whatsapp_message
+from src.config import Settings
 
 router = APIRouter(
     prefix="/api/v1/webhook",
@@ -15,15 +16,8 @@ router = APIRouter(
 @router.post("/", dependencies=[Depends(signature_required)])
 async def handle_message(request: Request, payload: WebhookPayload):
     body = payload.dict()
-
-    if (
-        body.get("entry", [{}])[0]
-        .get("changes", [{}])[0]
-        .get("value", {})
-        .get("statuses")
-    ):
+    if body.get("entry", [{}])[0].get("changes", [{}])[0].get("value", {}).get("statuses"):
         return {"status": "ok"}
-
     try:
         if is_valid_whatsapp_message(body):
             process_whatsapp_message(body)
@@ -36,7 +30,7 @@ async def handle_message(request: Request, payload: WebhookPayload):
 
 @router.get("/")
 async def verify(hub_mode: str, hub_verify_token: str, hub_challenge: str):
-    config = router.state.config
+    config = Settings()
     if hub_mode and hub_verify_token:
         if hub_mode == "subscribe" and hub_verify_token == config.VERIFY_TOKEN:
             return hub_challenge
